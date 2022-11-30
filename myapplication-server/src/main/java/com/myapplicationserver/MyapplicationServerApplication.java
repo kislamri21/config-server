6a1d87a84f76afb560c370f87253ee12c7fd41ee
kislamri <kamrulriad75@gmail.com>
2022-11-29 21:54:12 -0500

    added mvn project

---
+++ myapplication-server/src/main/java/com/myapplicationserver/MyapplicationServerApplication.java
@@ -0,0 +1,13 @@
+package com.myapplicationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyapplicationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyapplicationServerApplication.class, args);
+	}
+
+}
---
+++ myapplication-server/src/test/java/com/myapplicationserver/MyapplicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapplicationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyapplicationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
