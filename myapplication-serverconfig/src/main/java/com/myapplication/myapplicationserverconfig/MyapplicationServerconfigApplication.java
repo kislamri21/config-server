8476aaebbae1039efd2736e9c62078c87816f79b
kislamri <kamrulriad75@gmail.com>
2022-11-29 23:04:25 -0500

    Revert "git commit updated"
    This reverts commit 16233f705035ed4cebbd33d2afbec832c39880df.

--- myapplication-serverconfig/src/main/java/com/myapplication/myapplicationserverconfig/MyapplicationServerconfigApplication.java
+++ myapplication-serverconfig/src/main/java/com/myapplication/myapplicationserverconfig/MyapplicationServerconfigApplication.java
@@ -1,6 +1,5 @@
 package com.myapplication.myapplicationserverconfig;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyapplicationServerconfigApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(MyapplicationServerconfigApplication.class, args);
-		System.out.println("Spring boot hello world ............");
 	}
 
 }
