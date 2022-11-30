16233f705035ed4cebbd33d2afbec832c39880df
kislamri <kamrulriad75@gmail.com>
2022-11-29 23:01:57 -0500

    git commit updated

--- myapplication-serverconfig/src/main/java/com/myapplication/myapplicationserverconfig/MyapplicationServerconfigApplication.java
+++ myapplication-serverconfig/src/main/java/com/myapplication/myapplicationserverconfig/MyapplicationServerconfigApplication.java
@@ -1,5 +1,6 @@
 package com.myapplication.myapplicationserverconfig;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyapplicationServerconfigApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MyapplicationServerconfigApplication.class, args);
+		System.out.println("Spring boot hello world ............");
 	}
 
 }
