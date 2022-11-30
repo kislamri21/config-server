f6f11376a532f3f110723a39dadc015ec7fdf2dc
kislamri <kamrulriad75@gmail.com>
2022-11-29 22:11:49 -0500

    serverconfig addes

---
+++ myapplication-serverconfig/src/main/java/com/myapplication/myapplicationserverconfig/MyapplicationServerconfigApplication.java
@@ -0,0 +1,13 @@
+package com.myapplication.myapplicationserverconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyapplicationServerconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyapplicationServerconfigApplication.class, args);
+	}
+
+}
---
+++ myapplication-serverconfig/src/test/java/com/myapplication/myapplicationserverconfig/MyapplicationServerconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapplication.myapplicationserverconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyapplicationServerconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
